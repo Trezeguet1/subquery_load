77cdda137e76c2db9b82854bb2f8661a3c147fd4
vladislav <vladislav@MacBook-Pro.local>
2022-04-27 14:44:50 +0300

    change docker-compose

--- src/locust_run.py
+++ src/locust_run.py
@@ -9,7 +9,7 @@ from fixtures import stake_changes_by_address, history_changes_by_address
 
 class QuickstartUser(HttpUser):
     wait_time = constant(float(os.environ.get('WAIT_TIME', 1)))
-    host = "https://api.subquery.network/sq/ef1rspb"
+    host = "https://api.subquery.network/sq/soramitsu"
     headers = {"content-type": "application/json",
                "user-agent": "fearless/1 CFNetwork/978.0.7 Darwin/20.6.0",
                }
@@ -19,10 +19,10 @@ class QuickstartUser(HttpUser):
     @task
     def stake_changes(self):
         data = json.dumps(stake_changes_by_address(self.address))
-        self.client.post('/fearless-wallet', data=data, headers=self.headers)
+        self.client.post('/fearless-wallet-dot', data=data, headers=self.headers)
 
     @tag('history_elements')
     @task
     def history_elements(self):
         data = json.dumps(history_changes_by_address(self.address))
-        self.client.post('/fearless-wallet', data=data, headers=self.headers)
+        self.client.post('/fearless-wallet-dot', data=data, headers=self.headers)
